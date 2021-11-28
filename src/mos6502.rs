f953f16c4b7a03007403a1376c91254a2718602a
Paweł Baturo <pbaturo@gmail.com>
2021-11-28 13:26:10 +0100

    Test added for soft_reset function

--- src/mos6502.rs
+++ src/mos6502.rs
@@ -9,7 +9,7 @@ pub struct Mos6502 {
     ps_reg : u8,
 
     mem : [u8; MEM_SIZE_64K]
-}    
+}       
 
 impl Mos6502 {
     pub fn new() -> Self {
@@ -22,7 +22,7 @@ impl Mos6502 {
             ps_reg : 0,
             mem : [0; MEM_SIZE_64K]
         }
-    }
+    }      
 }
 
 pub fn soft_reset(cpu : &mut Mos6502) {
@@ -32,4 +32,35 @@ pub fn soft_reset(cpu : &mut Mos6502) {
     cpu.sp_reg = 0;
     cpu.pc_reg = 0;
     cpu.ps_reg = 0;
+}      
+
+#[cfg(test)]
+mod tests { 
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn should_soft_reset_function_set_registers_values_to_default() {
+        //Arrange
+        let mut cpu = Mos6502::new();
+        cpu.a_reg = 3;
+        cpu.x_reg = 24;
+        cpu.y_reg = 222;
+        cpu.sp_reg = 1;
+        cpu.pc_reg = 45;
+        cpu.ps_reg = 124;
+        //Act
+        soft_reset(&mut cpu);
+        //Assert
+        assert_eq!(cpu.a_reg, 0);
+        assert_eq!(cpu.x_reg, 0);
+        assert_eq!(cpu.y_reg, 0);
+        assert_eq!(cpu.sp_reg, 0);
+        assert_eq!(cpu.pc_reg, 0);
+        assert_eq!(cpu.ps_reg, 0);
+    }
 }
